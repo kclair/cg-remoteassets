422ba48342c22096cff8703dc5ab432e2151e787
K Clair <kclair@durga.serve.com>
2010-11-16 22:10:43 +0300

    fixes #2602 - activeresource not importing json data correctly

--- test/unit/remote_asset_test.rb
+++ test/unit/remote_asset_test.rb
@@ -4,7 +4,7 @@ class RemoteAssetTest < ActiveSupport::TestCase
 
   def test_create
     puts 'in test_create'
-    asset = RemoteAsset.new(:asset=>{:mime_from => 'txt/html', :filename => 'foofile'}.to_json)
+    asset = RemoteAsset.new(:mime_from => 'txt/html', :filename => 'foofile')
     asset.save
     assert_not_nil asset
   end
@@ -14,9 +14,13 @@ class RemoteAssetTest < ActiveSupport::TestCase
     @source = @asset.sources.find(1)
     # if i were a functional test i could call a controller that uses source_url but i'm not a functional controller
     url = "http://localhost:3000/source/#{@source.id}"
-    remote = RemoteAsset.new(:asset => {:mime_from => @asset.mime_type, :filename => @source.filename, :source_url => url}.to_json)
+    remote = RemoteAsset.new(:mime_from => @asset.mime_type, :filename => @source.filename, :source_url => url)
     remote.save
     assert_not_nil remote
+
+    # do a simple get to verify that the model is loading normally 
+    show_asset = RemoteAsset.find(remote.id)
+    assert_equal @asset.mime_type, show_asset.mime_from
   end
 
 end
