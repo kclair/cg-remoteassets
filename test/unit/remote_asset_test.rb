21d5aae6d0e9236a5c8d019805ac2f9aebd9b198
K Clair <kclair@pickledradish.com>
2010-12-30 09:36:45 -0700

    add unit tests to test remote statuses

--- test/unit/remote_asset_test.rb
+++ test/unit/remote_asset_test.rb
@@ -3,9 +3,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 class RemoteAssetTest < ActiveSupport::TestCase
 
   def test_create
-    puts 'in test_create'
-    asset = RemoteAsset.new(:mime_from => 'txt/html', :filename => 'foofile')
-    asset.save
+    asset = RemoteAsset.create(:mime_from => 'txt/html', :filename => 'foofile')
     assert_not_nil asset
   end
 
@@ -14,13 +12,53 @@ class RemoteAssetTest < ActiveSupport::TestCase
     @source = @asset.sources.find(1)
     # if i were a functional test i could call a controller that uses source_url but i'm not a functional controller
     url = "http://localhost:3000/source/#{@source.id}"
-    remote = RemoteAsset.new(:mime_from => @asset.mime_type, :filename => @source.filename, :source_url => url)
-    remote.save
+    remote = RemoteAsset.create(:mime_from => @asset.mime_type, :filename => @source.filename, :source_url => url)
     assert_not_nil remote
 
     # do a simple get to verify that the model is loading normally 
     show_asset = RemoteAsset.find(remote.id)
     assert_equal @asset.mime_type, show_asset.mime_from
+  end
+
+  def test_remote_asset_queued
+    @asset = assets(:one)
+    @source = @asset.sources.find(1)
+    url = "http://localhost:3000/source/#{@source.id}"
+    remote = RemoteAsset.create(:mime_from => @asset.mime_type, :filename => @source.filename, :source_url => url)
+    assert_not_nil remote
+    remote.reload
+    assert remote.status, 'newly created remote asset should get a status"'
+  end
+
+  def test_remote_asset_invalid_source
+    @asset = assets(:one)
+    remote = RemoteAsset.create(:mime_from => @asset.mime_type, :filename => 'foo', :source_url => 'http://foobar')
+    assert_not_nil remote
+    count = 0
+    until count == 30
+      remote.reload
+      break if remote.status == 'failed'
+      count = count+1
+      sleep 1
+    end
+    assert remote.status == 'failed', 'asset with invalid source url should fail'
+  end
+
+  def test_remote_asset_processed
+    @asset = assets(:one)
+    @source = @asset.sources.find(1)
+    url = "http://localhost:3000/source/#{@source.id}"
+    remote = RemoteAsset.create(:mime_from => @asset.mime_type, :filename => @source.filename, :source_url => url)
+    assert_not_nil remote
+    # give it 30 seconds to finish
+    count = 0
+    until count == 30
+      remote.reload
+      break if remote.status == 'succeeded'
+      count = count+1
+      sleep 1
+    end
+    assert remote.status == 'succeeded', 'asset with valid source url should succeed'
   end
 
 end
